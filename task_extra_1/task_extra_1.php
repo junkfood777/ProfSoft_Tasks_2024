7f1576cda65c657b6353f2ba37f0904950364629
junkfood777 <99825146+junkfood777@users.noreply.github.com>
2024-05-14 19:25:15 +0400

    Extra Task-1 is done

---
+++ task_extra_1/task_extra_1.php
@@ -0,0 +1,38 @@
+<?php
+
+function isCorrectBracketSequence($input) {
+    $stack = [];
+    $bracketPairs = [
+        ')' => '(',
+        '}' => '{',
+        ']' => '['
+    ];
+
+    for ($i = 0; $i < strlen($input); $i++) {
+        $char = $input[$i];
+
+        if (in_array($char, ['(', '{', '['])) {
+            array_push($stack, $char);
+        }
+        elseif (in_array($char, [')', '}', ']'])) {
+            if (empty($stack) || $stack[count($stack) - 1] !== $bracketPairs[$char]) {
+                return false;
+            } else {
+                array_pop($stack);
+            }
+        }
+    }
+
+    return empty($stack);
+}
+
+$inputFile = 'input.txt';
+$outputFile = 'output.txt';
+
+$input = file_get_contents($inputFile);
+
+$result = isCorrectBracketSequence($input);
+
+file_put_contents($outputFile, $result ? 'правильно' : 'не правильно');
+
+?>
